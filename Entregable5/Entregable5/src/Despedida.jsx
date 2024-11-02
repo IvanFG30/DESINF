d174aa6a57d4849535c9d51c1c8e2e44395255d2
Iván Fernández González <ivanfg2030@gmail.com>
2024-11-02 08:48:25 +0100

    Sincronizando entregable5 con repo remoto

---
+++ Entregable5/Entregable5/src/Despedida.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function Despedida({ nombre }) {
+  return <h1>¡Adiós, {nombre}!</h1>;
+}
+
+export default Despedida;
---
+++ Entregable5/Entregable5/src/Saludo.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function Saludo({ nombre }) {
+  return <h1>¡Hola, {nombre}!</h1>;
+}
+
+export default Saludo;
